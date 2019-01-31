5670509239701c4c84ac6235c4d9fe6e9e5e7210
LJXSAM <47037294+LJXSAM@users.noreply.github.com>
2019-01-30 22:16:14 -0500

    Add files via upload

---
+++ read 1.py
@@ -0,0 +1,15 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+		
+print('档案读取完了，总共有', len(data), '笔资料') 
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
