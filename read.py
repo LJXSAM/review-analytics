d0c10c162aa2f6cdaf297d17c63c552b9c82675c
LJXSAM <47037294+LJXSAM@users.noreply.github.com>
2019-01-30 21:50:20 -0500

    Add files via upload

--- read.py
+++ read.py
@@ -7,8 +7,12 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 		
-print(len(data))
+print('档案读取完了，总共有', len(data), '笔资料') 
 
-print(data[0])
-print('------------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
+
+print('留言的平均长度为', sum_len/len(data))
+ 
